c5ddf5da9c653e72d8ff2678fe9d811cabc2b8c0
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-09-22 20:00:34 +0600

    Done

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const colors = require("colors");
 const app = require("./app");
 
 // database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-  console.log(`database connection is successful!`.green.bold);
-});
-// mongoose.connect(process.env.DATABASE).then(() => {
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
 //   console.log(`database connection is successful!`.green.bold);
 // });
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mh2ii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log(`database connection is successful!`.green.bold);
+  });
 
 // server
 const port = process.env.PORT || 5000;
